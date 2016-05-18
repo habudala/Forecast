ceb55f37e278d584882956f852e80129d6872794
Laszlo Habuda <habudala@live.com>
2016-05-18 04:04:29 -0400

    Finalizing the Main Page

--- scripts/script.js
+++ scripts/script.js
@@ -1,114 +1,126 @@
 $(document).ready(function(){
 
 	var baseURL = "http://api.wunderground.com/api/";
-var apiKey = "004ee43afc5c6206";
-var input;
-var fullURL;
-var state;
-var mySubmit = document.getElementById("button");
-mySubmit.addEventListener("click", form, false);
+	var apiKey = "004ee43afc5c6206";
+	var input;
+	var fullURL;
+	var state;
+	var mySubmit = document.getElementById("button");
+	mySubmit.addEventListener("click", form, false);
 
-function form (e){
+	function form (e){
 
-	e.preventDefault();
-/////////////////////////////CLIENT INPUT STRING MANIPULATION///////////////////////////////////////
-	input = document.getElementById("input").value;
-	input = input.trim();
-	firstLetter = input.substring(0,1);
-	otherLetters = input.substring(1);
-	firstLetter = firstLetter.toUpperCase();
-	input = firstLetter + otherLetters;
-	
-	var space = input.search(" ");
+		e.preventDefault();
+	/////////////////////////////CLIENT INPUT STRING MANIPULATION///////////////////////////////////////
+		input = document.getElementById("input").value;
+		input = input.trim();
+		firstLetter = input.substring(0,1);
+		otherLetters = input.substring(1);
+		firstLetter = firstLetter.toUpperCase();
+		input = firstLetter + otherLetters;
+		
+		var space = input.search(" ");
 
-	if (space > -1) {
-		 secLetterPos = space +1;
-		 secLetter = input.substring(secLetterPos, secLetterPos + 1);
-		 secLetter = secLetter.toUpperCase();
-		 rest = input.slice(secLetterPos +1);
-		 newSecWord = secLetter + rest;
-		 firstWord = input.slice(0,space);
-		 input = firstWord + " " + newSecWord;
-		  
-	}
-	input = input.replace(/ /g,"_");
-	//console.log(input); // I WORK!!!
+		if (space > -1) {
+			 secLetterPos = space +1;
+			 secLetter = input.substring(secLetterPos, secLetterPos + 1);
+			 secLetter = secLetter.toUpperCase();
+			 rest = input.slice(secLetterPos +1);
+			 newSecWord = secLetter + rest;
+			 firstWord = input.slice(0,space);
+			 input = firstWord + " " + newSecWord;
+			  
+		}
+		input = input.replace(/ /g,"_");
+		//console.log(input); // I WORK!!!
 
-/////////////////////////////RETRIEVING JSON FROM UNDERGROUND WEATHER////////////////////////////////
-	state = document.getElementById("selected").value;
-	fullURL = baseURL + apiKey + "/conditions/forecast/q/" + state + "/" + input + ".json";
+	/////////////////////////////RETRIEVING JSON FROM UNDERGROUND WEATHER////////////////////////////////
+		state = document.getElementById("selected").value;
+		fullURL = baseURL + apiKey + "/conditions/forecast/q/" + state + "/" + input + ".json";
 
-	function getData () { //console.log("I start"); //self check
-		$.ajax({
-			type: "GET",
-			url: fullURL,
-			timeout: 3000,
-			success: function(data) {
-				console.log(data); //self check
-
-			},
-			fail: function() {
-				//console.log("fail sucka!"); //self check
-			}
-
-		}); 
-	}
-	getData();
-}
-
-
-function thumbInfo () {
-	//api.openweathermap.org/data/2.5/weather?q=London
-// api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1111111111
-	var baseURL = "http://api.openweathermap.org/data/2.5/";
-	var apiKey = "&APPID=8bc0abd3d79d29fd03b0be4c4fbe5b71";
-	var fullUrlI; //current conditions
-
-	var mainCities =["San_Francisco","Los_Angeles","New_York","WashingtonDC"];
-	
-	for(var i = 0; i < mainCities.length; i++) {
-		fullUrlI = baseURL + "weather?q=" + mainCities[i] + "=metric" + apiKey; //metric
-		fullUrlI = baseURL + "weather?q=" + mainCities[i] + "=imperial" + apiKey; //imperial
-
-		function getData2 () { console.log("I start"); //self check
-			var currTile = "tile" + (i+1);
-			var thisTile = document.getElementById(currTile);
-
+		function getData () { //console.log("I start"); //self check
 			$.ajax({
 				type: "GET",
-				url: fullUrlI,
+				url: fullURL,
 				timeout: 3000,
 				success: function(data) {
+					console.log(data); //self check
 
-				console.log(data); //self check
-
-				var thumbCity = data.name;
-				var thumbTempKel = data.main.temp;
-				var thumbTempCel = thumbTempKel - 273.15;
-				var thumbTempFar = thumbTempCel * 1.8 + 32;
-				var thumbMainDesc = data.weather[0].description;
-				var thumbIconCode = data.weather[0].icon;
-				var thumbIconUrl = "http://openweathermap.org/img/w/" + thumbIconCode + ".png";
-
-				//http://openweathermap.org/img/w/10d.png
-
-				thisTile.innerHTML = "<div id='tname'>" + thumbCity + "</div><br/><div id='timg'><img src='" +
-				thumbIconUrl +"' /></div><br/><div id='tdesc'>" + thumbMainDesc + 
-				"</div><div id='ttemp'>Temperature: " +
-			    thumbTempCel + "/ " + thumbTempFar + "</div>" 
+				 targetLoc = data.current_observation.display_location.full;
+				 targetIcon = data.current_observation.icon_url;
+				 iconDesc = data.current_observation.weather;
+				 currTempCel = data.current_observation.temp_c;
+				 currTempFar = data.current_observation.temp_f;
+				 maincontent = $("#maincontent");
+				
+				maincontent.html("<div id='location'>" + targetLoc + "</div>" + 
+				"<br/>" + "<div id='mimg'><img src='" +targetIcon +
+				"'/></div><br/><div id='mdesc'>" + iconDesc +
+				 "</div><br/><div id='mtemp'><h4>Temperature: </h4>" +
+				currTempCel + "&#8451;<br/>" + currTempFar + "&#8457;</div>");
 				},
 				fail: function() {
-				//console.log("fail sucka!"); //self check
+					//console.log("fail sucka!"); //self check
 				}
-			});
+
+			}); 
 		}
-	
-	getData2();
-
+		getData();
 	}
-}
 
-thumbInfo();
+
+	function thumbInfo () {
+		//api.openweathermap.org/data/2.5/weather?q=London
+	// api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1111111111
+		var baseURL = "http://api.openweathermap.org/data/2.5/";
+		var apiKey = "&APPID=8bc0abd3d79d29fd03b0be4c4fbe5b71";
+		var fullUrlI; //current conditions
+
+		var mainCities =["San_Francisco","Los_Angeles","New_York","WashingtonDC"];
+		
+		for(var i = 0; i < mainCities.length; i++) {
+			fullUrlI = baseURL + "weather?q=" + mainCities[i] + "=metric" + apiKey; //metric
+			fullUrlI = baseURL + "weather?q=" + mainCities[i] + "=imperial" + apiKey; //imperial
+
+			function getData2 () { console.log("I start"); //self check
+				var currTile = "tile" + (i+1);
+				var thisTile = document.getElementById(currTile);
+
+				$.ajax({
+					type: "GET",
+					url: fullUrlI,
+					timeout: 3000,
+					success: function(data) {
+
+					//console.log(data); //self check
+
+					var thumbCity = data.name;
+					var thumbTempKel = data.main.temp;
+					var thumbTempCel = Math.round(Number(thumbTempKel - 273.15));
+					var thumbTempFar = Math.round(Number(thumbTempCel * 1.8 + 32));
+					var thumbMainDesc = data.weather[0].description;
+					var thumbIconCode = data.weather[0].icon;
+					var thumbIconUrl = "http://openweathermap.org/img/w/" + thumbIconCode + ".png";
+
+					//http://openweathermap.org/img/w/10d.png
+
+					thisTile.innerHTML = "<div id='tname'>" + thumbCity + "</div><br/><div id='timg'><img src='" +
+					thumbIconUrl +"' /></div><br/><div id='tdesc'>" + thumbMainDesc + 
+					"</div><div id='ttemp'>Temperature: " +
+				    thumbTempCel + "&#8451; / " + thumbTempFar + "&#8457;</div>" 
+					},
+					fail: function() {
+					//console.log("fail sucka!"); //self check
+					}
+				});
+			}
+		
+		getData2();
+
+		}
+	}
+
+	thumbInfo();
 
 
 
