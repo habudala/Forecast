873a586fecf1026ecb3c985d0e5103d9db4eaf92
Laszlo Habuda <habudala@live.com>
2016-05-18 17:59:02 -0400

    Cleaning Up and Beginning With the 10-Day Forecast

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
 	var baseURL = "http://api.wunderground.com/api/";
 	var apiKey = "004ee43afc5c6206";
 	var mySubmit = document.getElementById("button");
+	var links = document.getElementsByTagName("a");
+
+
 	var maincontent = $("#maincontent");
 	var retrData;
-
+	var retr2Data;
 	var dayDate;
 	var newWeekday;
 	var newImg;
@@ -14,16 +17,17 @@ $(document).ready(function(){
 	var lowTempDiv;
 
 
-
 	function form (e){
 
 		e.preventDefault();
 
 		var input;
-		var fullURL;
+		var fullURLI;
+		var input;
+		var fullURLII;
 		var state;
-
-	/////////////////////////////CLIENT INPUT STRING MANIPULATION///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////CLIENT INPUT STRING MANIPULATION///////////////////////////////////////
 		input = document.getElementById("input").value;
 		input = input.trim();
 		firstLetter = input.substring(0,1);
@@ -46,50 +50,80 @@ $(document).ready(function(){
 		input = input.replace(/ /g,"_");
 		//console.log(input); // I WORK!!!
 
-	/////////////////////////////RETRIEVING JSON FROM UNDERGROUND WEATHER////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////RETRIEVING JSON FROM UNDERGROUND WEATHER////////////////////////////////
 		state = document.getElementById("selected").value;
-		fullURL = baseURL + apiKey + "/conditions/forecast/q/" + state + "/" + input + ".json";
+		fullURLI = baseURL + apiKey + "/conditions/forecast/q/" + state + "/" + input + ".json";
+		fullURLII = baseURL + apiKey + "/forecast10day/q/" + state + "/" + input + ".json";
 		console.log(e.target);
-		//maincontent = $("#maincontent");
-		// classChange(e);
-		function getData2 (e) { //console.log("I start"); //self check
-			$.ajax({
-				type: "GET",
-				url: fullURL,
-				timeout: 3000,
-				success: function(data) {
-					console.log(data); //self check
-				    retrData = data;
+		
+		//http://api.wunderground.com/api/004ee43afc5c6206/forecast10day/q/CA/San_Francisco.json
 
-					 if(links[0].className == "active") {
-					 	runCurrent(data);
+		function getData (e) { 
+
+////////////////////////////CURRENT AND 4-DAY FORECAST CASE JSON//////////////////////////////////////////
+			//console.log("I start"); //self check
+
+			if ((links[0].className == "active") || (links[1].className == "active"))
+			{
+				$.ajax({
+					type: "GET",
+					url: fullURLI,
+					timeout: 3000,
+					success: function(data) {
+						console.log(data); //self check
+					    retrData = data;
+
+						 if(links[0].className == "active") {
+						 	runCurrent(data);
+						}
+						else 
+						{
+							runFourDay(data);
+						}
+
+					},
+					fail: function() {
+						alert("Sorry, something's gone terribly wrong. You should start running...");
 					}
-					else 
-					{
-						runFourDay(data);
 
+				}); 
+			} else {
+
+////////////////////////////////////////10-DAY FORECAST CASE JSON//////////////////////////////////////////
+			
+				$.ajax({
+					type: "GET",
+					url: fullURLII,
+					timeout: 3000,
+					success: function(data) {
+						console.log(data); //self check
+					    retr2Data = data;
+
+					},
+					fail: function() {
+						alert("Sorry, something's gone terribly wrong. You should start running...");
 					}
 
-				},
-				fail: function() {
-					//console.log("fail sucka!"); //self check
-				}
-
-			}); 
+				});
+			}
 		}
-		getData2();
+		getData();
 	}
 
 	mySubmit.addEventListener("click", form, false);
 
-//////////////////////////////////RETRIEVING JSON FROM OPENWEATHERMAP API/////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////RETRIEVING JSON FROM OPENWEATHERMAP API///////////////////////////////
+	
 	function thumbInfo () {
-		//api.openweathermap.org/data/2.5/weather?q=London
-	// api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1111111111
+
+/////////////////////api.openweathermap.org/data/2.5/weather?q=London/////// OPENWEATHER API FROMAT////
+////////////// api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1111111111////WITH API KEY
+
 		var baseURL = "http://api.openweathermap.org/data/2.5/";
 		var apiKey = "&APPID=8bc0abd3d79d29fd03b0be4c4fbe5b71";
 		var fullUrlI; //current conditions
-
 		var mainCities =["San_Francisco","Los_Angeles","New_York","WashingtonDC"];
 		
 		for(var i = 0; i < mainCities.length; i++) {
@@ -106,7 +140,6 @@ $(document).ready(function(){
 					url: fullUrlI,
 					timeout: 3000,
 					success: function(data) {
-
 					//console.log(data); //self check
 
 					var thumbCity = data.name;
@@ -118,7 +151,7 @@ $(document).ready(function(){
 					var thumbIconUrl = "http://openweathermap.org/img/w/" + thumbIconCode + ".png";
 					//console.log(thumbCity);
 
-					//http://openweathermap.org/img/w/10d.png
+		////////http:////openweathermap.org/img/w/10d.png////// open weather icon url/////
 
 					thisTile.innerHTML = "<div id='tname'>" + thumbCity + "</div><br/><div id='timg'><img src='" +
 					thumbIconUrl +"' /></div><br/><div id='tdesc'>" + thumbMainDesc + 
@@ -137,18 +170,17 @@ $(document).ready(function(){
 	}
 
 	thumbInfo();
-	var links = document.getElementsByTagName("a");
-	var myNavBar = document.getElementById("navbar");
-	//console.log(myNavBar);
-	myNavBar.addEventListener("click", classChange, false);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////LINK DETECTION AND DISPLAY INFO CHANGE ENGINEERING//////////////////////////////
+	
 
 	function classChange (e) {
 
 		var target = e.target;
 		var currClass = target.getAttribute("class");
 		var currActClass = document.getElementsByClassName("active")[0];
-		// var links = document.getElementsByTagName("a");
-
+		
 		
 		if (currClass == "active") {
 			//console.log("wassuuup??");
@@ -160,21 +192,12 @@ $(document).ready(function(){
 			//console.log(target)
 			}
 
+
 		if (links[0].className == "active") {
 			if(input.value != false) {runCurrent(retrData);} else {console.log("empty1");}
 		}
 		else if(links[1].className == "active") {
-			if(input.value != false) {
-				// for (var j = 0; j < specsArr.length; j++) {
-				// 		 		newDiv.appendChild(specsArr[j]);
-				// 		 		console.log(specsArr.length);
-				// 		 		maincontent.append(newDiv);
-				// }
-				runFourDay(retrData);
-						 	
-			}		 	// maincontent.append(newDiv);}
-			else 
-			{console.log("empty2");}
+			if(input.value != false) {runFourDay(retrData);}else {console.log("empty2");}
 		}
 		else if(links[2].className == "active") {
 			if(input.value != false) {console.log("pop field3");} else {console.log("empty3");}
@@ -184,6 +207,12 @@ $(document).ready(function(){
 		}
 
 	}
+
+	var myNavBar = document.getElementById("navbar");
+	myNavBar.addEventListener("click", classChange, false);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////RETRIEVED JSON DATA MANIPULATION I.//////////////////////////////////////
 
 	function runCurrent (retrData) {
 			//console.log("I run too")
@@ -195,48 +224,6 @@ $(document).ready(function(){
 					 currTempCel = retrData.current_observation.temp_c;
 					 currTempFar = retrData.current_observation.temp_f;
 					 maincontent = $("#maincontent");
-
-////////////////////////////////////////////////////////////////////
-					
-						// for(i = 0; i < retrData.forecast.simpleforecast.forecastday.length; i++) {
-						// 	currDay = retrData.forecast.simpleforecast.forecastday[i];
-
-						// 	 var month = currDay.date.monthname;
-						// 	 var date = currDay.date.day;
-						// 	 var weekDay = currDay.date.weekday;
-
-						// 	 var foreIconDesc = currDay.conditions;
-						// 	 var foreIconUrl = currDay.icon_url;
-
-						// 	 var highTempCel = currDay.high.celsius;
-						// 	 var highTempFar = currDay.high.fahrenheit;
-
-						// 	 var lowTempCel = currDay.low.celsius;
-						// 	 var lowTempFar = currDay.low.fahrenheit;
-						// // 	 // console.log(document.getElementById("day" + (i+1)));
-
-						//  	 newDiv = document.createElement("div");
-						//  	 dayDate = document.createElement("h3");
-						//  	 newWeekday = document.createElement("h3");
-						//  	 newImg = document.createElement("img");
-						//  	 newDesc = document.createElement("h4");
-						//  	 highTempDiv = document.createElement("h4");
-						//  	 lowTempDiv = document.createElement("h4");
-				
-
-						//  	newDiv.className = "fourdayfore";
-						//  	newImg.setAttribute("src",foreIconUrl);
-						//  	dayDate.innerHTML = month + " " + date;
-						//  	newWeekday.innerHTML = weekDay;
-						//  	newDesc.innerHTML = foreIconDesc;
-						//  	highTempDiv.innerHTML = "High: " + highTempCel +"&#8451; / " + highTempFar + "&#8457;<br/>";
-						//  	lowTempDiv.innerHTML = "Low; " + lowTempCel +"&#8451; / " + lowTempFar + "&#8457;";
-						 	
-
-						//  	specsArr = [dayDate,newWeekday,newImg,newDesc,highTempDiv,lowTempDiv];
-						//  }
-
-//////////////////////////////////////////////////////////////////////////////
 					
 					maincontent.html("<div id='location'>" + targetLoc + "</div>" + 
 					"<br/>" + "<div id='mimg'><img src='" +targetIcon +
@@ -245,87 +232,64 @@ $(document).ready(function(){
 					currTempCel + "&#8451;<br/>" + currTempFar + "&#8457; <h4></div>");
 	}
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////RETRIEVED JSON DATA MANIPULATION II. ////////////////////////////////////
+
 	function runFourDay (retrData) {
 
-		maincontent.html("");
-						//  //console.log("second condition");
-						for(i = 0; i < retrData.forecast.simpleforecast.forecastday.length; i++) {
-							currDay = retrData.forecast.simpleforecast.forecastday[i];
+			maincontent.html("");
+					
+			for(i = 0; i < retrData.forecast.simpleforecast.forecastday.length; i++) {
+				currDay = retrData.forecast.simpleforecast.forecastday[i];
 
-							 var month = currDay.date.monthname;
-							 var date = currDay.date.day;
-							 var weekDay = currDay.date.weekday;
+				var month = currDay.date.monthname;
+				var date = currDay.date.day;
+				var weekDay = currDay.date.weekday;
 
-							 var foreIconDesc = currDay.conditions;
-							 var foreIconUrl = currDay.icon_url;
+				var foreIconDesc = currDay.conditions;
+				var foreIconUrl = currDay.icon_url;
 
-							 var highTempCel = currDay.high.celsius;
-							 var highTempFar = currDay.high.fahrenheit;
+				var highTempCel = currDay.high.celsius;
+				var highTempFar = currDay.high.fahrenheit;
 
-							 var lowTempCel = currDay.low.celsius;
-							 var lowTempFar = currDay.low.fahrenheit;
-						// 	 // console.log(document.getElementById("day" + (i+1)));
+				var lowTempCel = currDay.low.celsius;
+				var lowTempFar = currDay.low.fahrenheit;
+						
 
-						 	 newDiv = document.createElement("div");
-						 	 dayDate = document.createElement("h3");
-						 	 newWeekday = document.createElement("h3");
-						 	 newImg = document.createElement("img");
-						 	 newDesc = document.createElement("h4");
-						 	 highTempDiv = document.createElement("h4");
-						 	 lowTempDiv = document.createElement("h4");
+				newDiv = document.createElement("div");
+				dayDate = document.createElement("h3");
+				newWeekday = document.createElement("h3");
+				newImg = document.createElement("img");
+				newDesc = document.createElement("h4");
+				highTempDiv = document.createElement("h4");
+				lowTempDiv = document.createElement("h4");
 				
 
-						 	newDiv.className = "fourdayfore";
-						 	newImg.setAttribute("src",foreIconUrl);
-						 	dayDate.innerHTML = month + " " + date;
-						 	newWeekday.innerHTML = weekDay;
-						 	newDesc.innerHTML = foreIconDesc;
-						 	highTempDiv.innerHTML = "High: " + highTempCel +"&#8451; / " + highTempFar + "&#8457;<br/>";
-						 	lowTempDiv.innerHTML = "Low; " + lowTempCel +"&#8451; / " + lowTempFar + "&#8457;";
+				newDiv.className = "fourdayfore";
+				newImg.setAttribute("src",foreIconUrl);
+				dayDate.innerHTML = month + " " + date;
+				newWeekday.innerHTML = weekDay;
+				newDesc.innerHTML = foreIconDesc;
+				highTempDiv.innerHTML = "High: " + highTempCel +"&#8451; / " + highTempFar + "&#8457;<br/>";
+				lowTempDiv.innerHTML = "Low; " + lowTempCel +"&#8451; / " + lowTempFar + "&#8457;";
 						 	
 
-						 	specsArr = [dayDate,newWeekday,newImg,newDesc,highTempDiv,lowTempDiv];
+				specsArr = [dayDate,newWeekday,newImg,newDesc,highTempDiv,lowTempDiv];
 						 
-						 	for (var j = 0; j < specsArr.length; j++) {
-						 		newDiv.appendChild(specsArr[j]);
-						 	}
+				for (var j = 0; j < specsArr.length; j++) {
+					newDiv.appendChild(specsArr[j]);
+				}
 						 	
-						 	maincontent.append(newDiv);
-
+				maincontent.append(newDiv);
 						
-						}
+			}
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 		
 });
 
-// NEXT STEPS
-//ADD EVENT LISTENER TO THE 4 DAY FORECAST LINK 
-// CREATE ACTIVE AND NONACTIVE CLASSES
-// IN FORM, IF 4 DAY IS ACTIVE & TEXTIMPUT=TRUE ELSE...
-// THEN WORRY ABOUT OTHER API CALLS
+
 
 
 
